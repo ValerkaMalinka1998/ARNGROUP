dcfb751aabafc02c9a5bd6dac87f6d5421049937
Valera <46463165+ValerkaMalinka1998@users.noreply.github.com>
2022-10-05 11:37:17 +0300

    Add files via upload

--- js/number.js
+++ js/number.js
@@ -1,23 +1,47 @@
-const total = document.querySelector('.input-number__input'),
-  min = 1,
-  max = 99;
+const total1 = document.querySelector('.input-number__input1'),
+  min1 = 1,
+  max1 = 99;
 
 // Уменьшаем на 1 
-document.querySelector('.input-number__minus').addEventListener('click', () => {
-  if (total.value > min) {
-    total.value = +total.value - 1;
+document.querySelector('.input-number__minus1').addEventListener('click', () => {
+  if (total1.value > min1) {
+    total1.value = +total1.value - 1;
   }
 });
 
 // Увеличиваем на 1 
-document.querySelector('.input-number__plus').addEventListener('click', () => {
-if (total.value < max) {
-  total.value = +total.value + 1;
+document.querySelector('.input-number__plus1').addEventListener('click', () => {
+if (total1.value < max1) {
+  total1.value = +total1.value + 1;
   }
 });
 
 // Запрещаем ввод текста 
-document.querySelectorAll('.input-number__input').forEach(function(el) {
+document.querySelectorAll('.input-number__input1').forEach(function(el) {
+  el.addEventListener('input', function() {
+    this.value = this.value.replace(/[^d]/g, '');
+  });
+});
+
+const total2 = document.querySelector('.input-number__input2'),
+  min2 = 1,
+  max2 = 99;
+// Уменьшаем на 1 
+document.querySelector('.input-number__minus2').addEventListener('click', () => {
+  if (total2.value > min2) {
+    total2.value = +total2.value - 1;
+  }
+});
+
+// Увеличиваем на 1 
+document.querySelector('.input-number__plus2').addEventListener('click', () => {
+if (total2.value < max2) {
+  total2.value = +total2.value + 1;
+  }
+});
+
+// Запрещаем ввод текста 
+document.querySelectorAll('.input-number__input2').forEach(function(el) {
   el.addEventListener('input', function() {
     this.value = this.value.replace(/[^d]/g, '');
   });
